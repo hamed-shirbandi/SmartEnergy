f15efa746f065eba32cf0d2918b1bcafc5e10ec6
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 15:44:30 +0330

    chore: remove unused files

--- src/3-Clients/Dashboard/Program.cs
+++ src/3-Clients/Dashboard/Program.cs
@@ -1,11 +1,11 @@
-using Dashboard;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmartEnergy.Clients.Dashboard;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("Url:Consumptions-Client")) });
 
 await builder.Build().RunAsync();
