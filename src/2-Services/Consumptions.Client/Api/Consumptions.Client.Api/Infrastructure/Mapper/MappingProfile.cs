cb0e99f5d8440991eab8cd35396e3afd411613df
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 15:10:39 +0330

    feat: add mapper configs to infrastructure

---
+++ src/2-Services/Consumptions.Client/Api/Consumptions.Client.Api/Infrastructure/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SmartEnergy.BuildingBlocks.Contracts.Dtos;
+using SmartEnergy.BuildingBlocks.Contracts.Protos;
+
+namespace SmartEnergy.Services.Consumptions.Client.Api.Infrastructure.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<GetConsumptionsGrpcResponse, GetConsumptionDto>();
+        }
+    }
+}
