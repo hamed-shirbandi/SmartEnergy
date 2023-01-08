3f009526f5c89f9b06dd4e28ae0ea1834cb0d88a
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 14:21:10 +0330

    feat: add consumption domain model

---
+++ src/2-Services/Consumptions.Server/Api/Consumptions.Server.Api/Domain/Consumption.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartEnergy.Services.Consumptions.Server.Api.Domain
+{
+    public class Consumption
+    {
+        public string Time { get; set; }
+        public string MeterUsage { get; set; }
+    }
+}
