34f8a2e4026625ebab5351270d8de7d866c778a1
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 14:31:59 +0330

    feat: add a repository for consumption domain entity

---
+++ src/2-Services/Consumptions.Server/Api/Consumptions.Server.Api/Infrastructure/Repositories/ConsumptionReqpository.cs
@@ -0,0 +1,17 @@
+﻿using SmartEnergy.Services.Consumptions.Server.Api.Domain;
+using SmartEnergy.Services.Consumptions.Server.Api.Infrastructure.DbContext;
+
+namespace SmartEnergy.Services.Consumptions.Server.Api.Infrastructure.Repositories
+{
+    public class ConsumptionReqpository
+    {
+        public ConsumptionReqpository()
+        {
+        }
+
+        public async Task<IEnumerable<Consumption>> GetConsumptions(int skip, int take)
+        {
+            return TimeSeriesDb.Consumptions.Skip(skip).Take(take);
+        }
+    }
+}
