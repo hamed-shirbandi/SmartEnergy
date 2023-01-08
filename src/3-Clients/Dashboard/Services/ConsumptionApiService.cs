18bcf13499734e7013337cf61b9ef6acea5994a1
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 15:31:41 +0330

    feat: add ConsumptionApiService to get data from consumptions client

---
+++ src/3-Clients/Dashboard/Services/ConsumptionApiService.cs
@@ -0,0 +1,48 @@
+﻿using SmartEnergy.BuildingBlocks.Contracts.Dtos;
+using System.Net.Http.Json;
+
+namespace SmartEnergy.Clients.Dashboard.Services
+{
+    public class ConsumptionApiService
+    {
+        #region Fields
+
+        private HttpClient _httpClient;
+
+        #endregion
+
+        #region Ctor
+
+        public ConsumptionApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<IEnumerable<GetConsumptionDto>> GetListAsync(int skip, int take)
+        {
+            var url = $"/consumptions/{skip}/{take}";
+            var httpResponse = await _httpClient.GetAsync(url);
+            return await httpResponse.Content.ReadFromJsonAsync<IEnumerable<GetConsumptionDto>>();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+    }
+}
