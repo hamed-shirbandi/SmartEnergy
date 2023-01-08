df325c4516f8e66952ed53fd35c5b8933787482d
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 15:14:28 +0330

    feat: add a caching behavior

---
+++ src/2-Services/Consumptions.Client/Api/Consumptions.Client.Api/Infrastructure/Behaviors/BehaviorsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace SmartEnergy.Services.Consumptions.Client.Api.Infrastructure.Behaviors
+{
+    public static class BehaviorsExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddCachingBehavior(this IServiceCollection services)
+        {
+            services.AddEasyCaching(option=>option.UseInMemory());
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
+        }
+
+
+    }
+}
---
+++ src/2-Services/Consumptions.Client/Api/Consumptions.Client.Api/Infrastructure/Behaviors/CachingBehavior.cs
@@ -0,0 +1,88 @@
+﻿using EasyCaching.Core;
+using MediatR;
+using System.Reflection;
+
+namespace SmartEnergy.Services.Consumptions.Client.Api.Infrastructure.Behaviors
+{
+
+    /// <summary>
+    /// Caching response for queries using Pipeline Pattern
+    /// </summary>
+    public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        #region Fields
+
+        private readonly IEasyCachingProvider _cachingProvider;
+        private readonly IConfiguration _configuration;
+
+        #endregion
+
+        #region Ctors
+
+
+        public CachingBehavior(IEasyCachingProvider cachingProvider, IConfiguration configuration)
+        {
+            _cachingProvider = cachingProvider;
+            _configuration = configuration;
+        }
+
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var configurationCacheEnabled = bool.Parse(_configuration["Caching:Enabled"]);
+            if (!configurationCacheEnabled)
+                return await next();
+
+            var cacheKey = GenerateKeyFromRequest(request);
+            var cachedResponse = await _cachingProvider.GetAsync<TResponse>(cacheKey);
+
+            if (cachedResponse.Value != null)
+                return cachedResponse.Value;
+
+            var response = await next();
+
+            var cacheTimeInMinutes = int.Parse(_configuration["Caching:CacheTimeInMinutes"]);
+            var expirationTime = DateTime.Now.AddMinutes(cacheTimeInMinutes);
+            await _cachingProvider.SetAsync(cacheKey, response, expirationTime.TimeOfDay);
+
+            return response;
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private string GenerateKeyFromRequest(TRequest request)
+        {
+            var properties = new List<PropertyInfo>(request.GetType().GetProperties());
+            var key = request.GetType().Name;
+
+            foreach (PropertyInfo property in properties)
+            {
+                object propValue = property.GetValue(request, property.GetIndexParameters());
+
+                string name = property.Name;
+                string value = propValue != null ? propValue.ToString() : "";
+                key += $"_{name}:{value}";
+            }
+
+            return key;
+        }
+
+
+        #endregion
+    }
+}
