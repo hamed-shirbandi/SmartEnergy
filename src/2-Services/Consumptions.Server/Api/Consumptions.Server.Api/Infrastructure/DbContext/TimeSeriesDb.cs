1f85af991302a1b6d9ff2664aade28e1a456305c
hamed shirbandi <hamed.shirbandi@gmail.com>
2023-01-08 14:27:46 +0330

    feat: add database configs to infrastructure

---
+++ src/2-Services/Consumptions.Server/Api/Consumptions.Server.Api/Infrastructure/DbContext/TimeSeriesDb.cs
@@ -0,0 +1,28 @@
+﻿using SmartEnergy.Services.Consumptions.Server.Api.Domain;
+using System.Text.Json;
+
+namespace SmartEnergy.Services.Consumptions.Server.Api.Infrastructure.DbContext
+{
+    /// <summary>
+    /// To fake the databse and get data from a text file
+    /// It sould be a real Time Serie Database like InfluxData on production
+    /// </summary>
+    public static class TimeSeriesDb
+    {
+
+        /// <summary>
+        /// Mocked Consumptions table
+        /// </summary>
+        public static IEnumerable<Consumption> Consumptions => GetConsumptionsFromTextFile();
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        private static IEnumerable<Consumption> GetConsumptionsFromTextFile()
+        {
+            string consumptionJsonText = File.ReadAllText(@"./Infrastructure/DB/meterusage.json");
+            return JsonSerializer.Deserialize<IEnumerable<Consumption>>(consumptionJsonText);
+        }
+    }
+}
